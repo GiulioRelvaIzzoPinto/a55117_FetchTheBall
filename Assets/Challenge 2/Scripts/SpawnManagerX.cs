404c98e408dd98d3fd523554b2372dcd93218d24
Giulio <giuliorelvaip@gmail.com>
2024-03-18 18:49:07 +0000

    7.Only one type of ball is being spawned

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -25,8 +25,8 @@ public class SpawnManagerX : MonoBehaviour
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
-        // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        int ballnumber = Random.Range (0, ballPrefabs.Length);
+        Instantiate(ballPrefabs[ballnumber], spawnPos, ballPrefabs[ballnumber].transform.rotation);
     }
 
 }
